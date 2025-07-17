9886e2e0fec6fb1682d33481b481c04c4fdfb7bd
TzCher <tsvetozar@telerikacademy.com>
2025-07-17 20:09:57 +0300

    add: Calculator functionality

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-const sum = (a, b) => {
-    return a + b;
-}
+
+const multiply = (a, b) => {
+    return a * b;
+}   
+
+const divide = (a, b) => {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+}   
